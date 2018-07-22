cccfcfa5dfd53ef1af45ba2bdc25c85cab5a266c
Son Luong Ngoc (Lazada Group) <son.luongngoc@LAZ-VN-L-M-1269.local>
2018-07-22 16:04:27 +0700

    small style fix

--- src/dingtalk/client.test.ts
+++ src/dingtalk/client.test.ts
@@ -3,8 +3,10 @@ import {DingtalkRobotClient} from "./client";
 import {MarkdownMessageBuilder} from "./message";
 
 const client = new DingtalkRobotClient(
-    // tslint:disable-next-line:max-line-length
-    new URL("https://oapi.dingtalk.com/robot/send?access_token=86134fe83a80ab52bab435a9a26e8d59becc5893a71b52e072c476e16602f73f"),
+    new URL(
+        "/robot/send?access_token=86134fe83a80ab52bab435a9a26e8d59becc5893a71b52e072c476e16602f73f",
+        "https://oapi.dingtalk.com",
+    ),
     new MarkdownMessageBuilder()
         .setTitle("Robot Message")
         .setText("\n\nHello World!")
